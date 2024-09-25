f9b9d3134822845732b0b52536d7626c122fd9f0
Riceball LEE <snowyu.lee@gmail.com>
2024-09-25 19:17:05 +0800

    chore: update

--- src/lib/init-tools.ts
+++ src/lib/init-tools.ts
@@ -4,13 +4,27 @@ import {
   ResServerTools,
   ServerTools,
 } from '@isdk/ai-tool'
-import { llm } from '@isdk/ai-tool-llm'
+import { llm, LLMProvider } from '@isdk/ai-tool-llm'
 import { LlamaCppProviderName, llamaCpp } from '@isdk/ai-tool-llm-llamacpp'
 import { AIPromptsFunc, AIPromptsName } from '@isdk/ai-tool-prompt'
 import { download } from '@isdk/ai-tool-downloader'
 import type { Hook, Config } from '@oclif/core'
 
 export const BRAINS_FUNC_NAME = 'llm.brains'
+
+const providers: {[name: string]: LLMProvider} = {}
+
+export function registerProvider(provider: LLMProvider) {
+  providers[provider.name!] = provider
+}
+
+function initRegisteredProviders() {
+  for (const provider of Object.values(providers)) {
+    provider.register()
+  }
+}
+
+registerProvider(llamaCpp)
 
 export async function initTools(this: Hook.Context, userConfig: any, _config: Config) {
   try {
@@ -19,8 +33,21 @@ export async function initTools(this: Hook.Context, userConfig: any, _config: Co
 
     ServerTools.register(promptsFunc)
     ServerTools.register(llm)
-    llamaCpp.register()
-    llm.setCurrentProvider(LlamaCppProviderName)
+
+    // llamaCpp.register()
+    initRegisteredProviders()
+
+    let currentProviderName = userConfig.provider || LlamaCppProviderName
+    const providerUriParts = currentProviderName.split('://')
+    currentProviderName = providerUriParts[0]
+    if (!providers[currentProviderName]) {throw new Error(`Provider ${currentProviderName} not found`)}
+
+    const provider = providers[currentProviderName]
+    llm.setCurrentProvider(currentProviderName)
+    if (providerUriParts[1]) {
+      provider.model = providerUriParts[1]
+    }
+    console.log('🚀 ~ initTools ~ provider.model:', provider.model)
 
     // the event-bus for server
     ResServerTools.register(event)
@@ -28,7 +55,10 @@ export async function initTools(this: Hook.Context, userConfig: any, _config: Co
     ResServerTools.register(download)
 
     if (userConfig.apiUrl) {
-      llamaCpp.apiUrl = userConfig.apiUrl
+      provider.apiUrl = userConfig.apiUrl
+    }
+    if (userConfig.apiKey) {
+      provider.apiKey = userConfig.apiKey
     }
   } catch (err) {
     console.error('🚀 ~ initTools ~ err:', err)
