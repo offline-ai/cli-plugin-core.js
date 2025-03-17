2a45a7457a6bd10b2e749ae9afe99902bf7529f4
Riceball LEE <snowyu.lee@gmail.com>
2025-03-17 08:39:43 +0800

    chore: update

--- src/oclif/hooks/init-tools.ts
+++ src/oclif/hooks/init-tools.ts
@@ -4,12 +4,12 @@ import { initTools as _initTools } from '../../lib/init-tools.js'
 
 let initialized: boolean
 export async function init_tools(this: Hook.Context, options: {userConfig: any, config: Config}) {
+  console.log('🚀 ~ file: init-tools.ts:7 ~ options:', options)
   if (initialized) return
   initialized = true
 
   await _initTools.call(this, options.userConfig, options.config)
   await this.config.runHook('register', {...options})
-  initialized = true
 }
 
 export default init_tools
