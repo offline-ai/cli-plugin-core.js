b87031fbd7c93b3a63698b5288858299385fa701
Riceball LEE <snowyu.lee@gmail.com>
2025-07-11 20:55:00 +0800

    feat: export initTools

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {run} from '@oclif/core'
 export { AIScriptEx, runScript, translate } from './lib/run-script.js'
-export { registerProvider } from './lib/init-tools.js'
+export { initTools, registerProvider } from './lib/init-tools.js'
